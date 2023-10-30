a38244f7c0df185c6e35861448c97d727170b90f
martynapiskorz <126684779+martynapiskorz@users.noreply.github.com>
2023-10-30 14:13:50 +0100

    Update dta.py

--- dta.py
+++ dta.py
@@ -1,10 +1,2 @@
 import pandas as pd
 import numpy as np
-
-def dta (file_dta):
-    data = pd.io.stata.read_stata(file_dta)
-    dta = data.to_csv(file_dta)
-    return dta
-
-wave1 = dta('ca08a_1.0p_EN.dta')
-print(wave1)
